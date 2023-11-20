3b2423c001ad17317e0fa5ac857b8ca2c4fe194b
PipeFlayy <fe.galan@duocuc.cl>
2023-11-20 12:56:48 -0300

    prima

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { DataService } from 'src/app/data.service'; // Importa tu servicio de datos
+import { DataService } from 'src/app/data.service';
 import { AuthService } from '../../services/auth.service';
-
 
 @Component({
   selector: 'app-profile',
@@ -10,25 +9,33 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  userData: any; // Variable para almacenar los datos del usuario
-  rol: string | null= '';
+  userData: any;
+  rol: string | null = '';
+  usuario: string | null = '';
+  correo: string | null = '';
 
   constructor(private router: Router, private dataService: DataService, private authService: AuthService) {}
 
-  async ngOnInit() {  //Crear un return en formulario para k no se vea la pagina
+  async ngOnInit() {
     let user = await this.authService.getCurrentUser();
     if (user && user.email) {
+      this.correo = localStorage.getItem('username');
+      if (this.correo) {
+        this.usuario = this.correo.split('@')[0];
+      }
       this.rol = localStorage.getItem(user.email);
     }
-    if (this.rol == null || this.rol == '')
-    {
-      this.router.navigate(['/formulario'])
-      console.log("No autorizado")
+
+    if (this.rol == null || this.rol == '') {
+      this.router.navigate(['/formulario']);
+      console.log("No autorizado");
+    } else {
+      this.getUserData(); 
     }
-}
+  }
 
   getUserData() {
-    // Suponiendo que tienes un método en tu servicio para obtener los datos del usuario
-    this.userData = this.dataService.getUser(); // Esta función podría variar según tu servicio
+    
+    this.userData = this.dataService.getUser(); 
   }
 }
