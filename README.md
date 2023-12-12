d1eaece688c405b6f5dd1b1bcbb6ad9c7287b9be
MiguelGaticaBestnickname123 <mi.gatica@duocuc.cl>
2023-12-12 02:55:10 -0300

    definitivo?

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# xXaAziStHenT3Xx
-### no me hackees?
+# README.md para la Aplicación Móvil de Asistencia (Ionic Angular)
 
-#### TANGANANICA PO
+## Descripción General
+Esta aplicación móvil desarrollada en Ionic Angular permite a los estudiantes registrar su asistencia capturando un código QR. Cada código QR contiene información de una asignatura y una fecha, siguiendo el formato `[asignatura@fecha]`. La aplicación separa estos datos y los procesa para llevar un conteo de asistencias, mostrándolas de manera interactiva en la interfaz de usuario.
+
+## Funcionalidades
+- **Registro de Asistencia con Código QR**: Los estudiantes capturan un código QR que registra automáticamente su asistencia.
+- **Conteo y Visualización de Asistencias**: La app cuenta y muestra las asistencias en la interfaz, por asignatura y fecha.
+- **Integración con Firebase**: Utiliza Firebase para la autenticación de usuarios y el almacenamiento de datos.
+
+## Imágenes
+- ![Interfaz de escaneo](https://drive.google.com/file/d/1QO6Atebzh7ewN2jjr7m889yih-aoqt0E/view?usp=drive_link)
+- ![Escaneo](https://drive.google.com/file/d/1Q_3H3pvawhPB89VjqMArUTOWjqu_HsMq/view?usp=drive_link)
+- ![Asistencias](https://drive.google.com/file/d/1QUQTThqqTrExwEAgdOeIcWbAtZ0IvOVH/view?usp=drive_link)
+- ![Despliegue de asistencia](https://drive.google.com/file/d/1QTPyom5F-P9khvz8H_0qLcdAs-V1kEIC/view?usp=drive_link)
+
+## Descargas y Código Fuente
+- **APK**: [Descargar la APK aquí](https://www.mediafire.com/file/roxbpxtuagqzpfq/app-debug.apk/file)
+- **Código Fuente**: [Repositorio en GitHub](https://github.com/MiguelGaticaBestNick123/app)
--- src/app/pages/alertas/alertas.page.ts
+++ src/app/pages/alertas/alertas.page.ts
@@ -1,55 +1,98 @@
-import { Component, OnInit,ChangeDetectorRef  } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
-import { Router, ActivatedRoute, } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
+class Asignatura {
+    nombre: string;
+    totalClases: number;
+    asistencias: number = 0;
 
+    constructor(nombre: string, totalClases: number) {
+        this.nombre = nombre;
+        this.totalClases = totalClases;
+    }
 
-@Component({
-  selector: 'app-alertas',
-  templateUrl: './alertas.page.html',
-  styleUrls: ['./alertas.page.scss'],
-})
-export class AlertasPage implements OnInit {
-  texto: string;
-  fecha: string;
-  asignatura: string;
-  registros: any[] = [];
-  constructor(private alertController: AlertController, private authService: AuthService, private route: ActivatedRoute, private router: Router, private changeDetector: ChangeDetectorRef) { }
-  
+    registrarAsistencia() {
+        this.asistencias++;
+    }
 
-
-  ngOnInit() {
-    let registrosLocalStorage = localStorage.getItem('registros');
-    this.registros = registrosLocalStorage ? JSON.parse(registrosLocalStorage) : []; 
-
-    this.route.queryParams.subscribe(params => {
-      const navigation = this.router.getCurrentNavigation();
-      this.texto = String(localStorage.getItem('texto')) || 'no encontrado'; 
-      if (this.texto && this.texto !== 'no encontrado') { 
-        if (navigation && navigation.extras.state) {
-          this.texto = navigation.extras.state['texto'];
-        }
-        let partes = this.texto.split('@');
-        this.asignatura = partes[0];
-        this.fecha = partes[1];
-        console.log(this.asignatura);
-        console.log(this.fecha);
-        this.registros.push({ asignatura: this.asignatura, fecha: this.fecha });
-        localStorage.setItem('registros', JSON.stringify(this.registros)); 
-        localStorage.removeItem('texto');
-        this.changeDetector.detectChanges(); 
-        console.log(JSON.stringify(this.registros+' registro'));
-      }
-
-    }); 
-  }
-
-
-
-
-
-
-
+    calcularPorcentaje(): number {
+        return (this.asistencias / this.totalClases) * 100;
+    }
 }
 
+@Component({
+    selector: 'app-alertas',
+    templateUrl: './alertas.page.html',
+    styleUrls: ['./alertas.page.scss'],
+})
+export class AlertasPage implements OnInit {
+    texto: string;
+    fecha: string;
+    asignatura: string;
+    registros: any[] = [];
+    asignaturas: { [key: string]: Asignatura };
+
+    constructor(private alertController: AlertController, private authService: AuthService, private route: ActivatedRoute, private router: Router, private changeDetector: ChangeDetectorRef) { }
+
+    ngOnInit() {
+        this.cargarAsignaturas();
+        this.cargarRegistros();
+
+        this.route.queryParams.subscribe(params => {
+            const navigation = this.router.getCurrentNavigation();
+            this.texto = String(localStorage.getItem('texto')) || 'no encontrado'; 
+            if (this.texto && this.texto !== 'no encontrado') { 
+                if (navigation && navigation.extras.state) {
+                    this.texto = navigation.extras.state['texto'];
+                }
+                let partes = this.texto.split('@');
+                this.asignatura = partes[0];
+                this.fecha = partes[1];
+
+                this.registrarAsistenciaAsignatura(this.asignatura);
+                this.registros.push({ asignatura: this.asignatura, fecha: this.fecha });
+                localStorage.setItem('registros', JSON.stringify(this.registros)); 
+                localStorage.removeItem('texto');
+                this.changeDetector.detectChanges(); 
+            }
+        }); 
+    }
+
+    cargarAsignaturas() {
+        let asignaturasLocalStorage = localStorage.getItem('asignaturas');
+        if (asignaturasLocalStorage) {
+            let asignaturasData = JSON.parse(asignaturasLocalStorage);
+            this.asignaturas = {};
+            for (let key in asignaturasData) {
+                let asignatura = asignaturasData[key];
+                this.asignaturas[key] = new Asignatura(asignatura.nombre, asignatura.totalClases);
+                this.asignaturas[key].asistencias = asignatura.asistencias;
+            }
+        } else {
+            this.asignaturas = {
+                'Programación movil': new Asignatura('Programación movil', 30),
+                'Programación web': new Asignatura('Programación web', 25),
+                'Programación de sistemas': new Asignatura('Programación de sistemas', 20),
+                'Programación de base de datos': new Asignatura('Programación de base de datos', 28)
+            };
+        }
+    }
+
+    cargarRegistros() {
+        let registrosLocalStorage = localStorage.getItem('registros');
+        this.registros = registrosLocalStorage ? JSON.parse(registrosLocalStorage) : []; 
+    }
+
+    registrarAsistenciaAsignatura(nombreAsignatura: string) {
+        if (this.asignaturas[nombreAsignatura]) {
+            this.asignaturas[nombreAsignatura].registrarAsistencia();
+            localStorage.setItem('asignaturas', JSON.stringify(this.asignaturas));
+        }
+    }
+
+    obtenerPorcentajeAsignatura(nombreAsignatura: string): number {
+        return this.asignaturas[nombreAsignatura] ? this.asignaturas[nombreAsignatura].calcularPorcentaje() : 0;
+    }
+}
